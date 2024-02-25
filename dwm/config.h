829e7aedcdcd79c18e974942b3fd1fd73c3bfa80
JirakLu <jirak.lukas@gmail.com>
2024-02-25 12:23:32 +0100

    feat(dwm): add controls to play/pause, next, prev function keys

--- dwm/config.h
+++ dwm/config.h
@@ -111,6 +111,9 @@ static const Key keys[] = {
         { 0, XF86XK_AudioMute,		spawn,		SHCMD("wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle; kill -44 $(pidof dwmblocks)") },
 	{ 0, XF86XK_AudioRaiseVolume,	spawn,		SHCMD("wpctl set-volume @DEFAULT_AUDIO_SINK@ 0%- && wpctl set-volume @DEFAULT_AUDIO_SINK@ 3%+; kill -44 $(pidof dwmblocks)") },
 	{ 0, XF86XK_AudioLowerVolume,	spawn,		SHCMD("wpctl set-volume @DEFAULT_AUDIO_SINK@ 0%+ && wpctl set-volume @DEFAULT_AUDIO_SINK@ 3%-; kill -44 $(pidof dwmblocks)") },
+	{ 0, XF86XK_AudioNext,	spawn,		SHCMD("playerctl next") },
+	{ 0, XF86XK_AudioPrev,	spawn,		SHCMD("playerctl prev") },
+	{ 0, XF86XK_AudioPlay,	spawn,		SHCMD("playerctl play-pause") },
 };
 
 /* button definitions */
