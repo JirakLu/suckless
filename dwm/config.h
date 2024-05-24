34e77e882fea1457181dbc5bb389f7ca4d64c0a9
JirakLu <jirak.lukas@gmail.com>
2024-05-24 10:56:34 +0200

    feat: add .gitignore, cleanup, init .idea project

--- dwm/config.h
+++ dwm/config.h
@@ -1,3 +1,4 @@
+#pragma once
 /* See LICENSE file for copyright and license details. */
 
 /* constants */
@@ -6,17 +7,18 @@
 #define TERMINAL "warp-terminal" 
 
 /* appearance */
-static const unsigned int refresh_rate = 144;    /* Matches dwm's mouse event processing to your monitor's refresh rate for smoother window interactions. */
-static const unsigned int borderpx  = 0;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int systraypinning = 1;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
-static const unsigned int systrayonleft = 0;    /* 0: systray in the right corner, >0: systray on left of status text */
-static const unsigned int systrayspacing = 2;   /* systray spacing */
-static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
-static const int showsystray        = 1;        /* 0 means no systray */
-static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
+static const unsigned int refresh_rate		= 144; /* Matches dwm's mouse event processing to your monitor's refresh rate for smoother window interactions. */
+static const unsigned int borderpx		= 0;   /* border pixel of windows */
+static const unsigned int snap			= 32;  /* snap pixel */
+static const unsigned int systraypinning	= 1;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayonleft		= 0;   /* 0: systray in the right corner, >0: systray on left of status text */
+static const unsigned int systrayspacing	= 2;   /* systray spacing */
+static const int systraypinningfailfirst	= 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray			= 1;   /* 0 means no systray */
+static const int swallowfloating		= 0;   /* 1 means swallow floating windows by default */
+static const int showbar			= 1;   /* 0 means no bar */
+static const int topbar				= 1;   /* 0 means bottom bar */
+
 static const char *fonts[]          = { "JetBrainsMono NF:size=10", "JoyPixels:size=10:antialias=true:autohint=true" };
 static const char dmenufont[]       = "JetBrainsMono NF:size=10";
 static const char col_gray1[]       = "#2E3440";
@@ -24,6 +26,7 @@ static const char col_gray2[]       = "#3B4252";
 static const char col_gray3[]       = "#D8DEE9";
 static const char col_gray4[]       = "#ECEFF4";
 static const char col_cyan[]        = "#434C5E";
+
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -46,10 +49,10 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
-static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
-static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
+static const float mfact	= 0.55; /* factor of master area size [0.05..0.95] */
+static const int nmaster	= 1;    /* number of clients in master area */
+static const int resizehints	= 1;    /* 1 means respect size hints in tiled resizals */
+static const int lockfullscreen = 1;    /* 1 will force focus on the fullscreen window */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
@@ -77,16 +80,16 @@ static const char *termcmd[]  = { TERMINAL, NULL };
 #include <X11/XF86keysym.h>
 
 static const Key keys[] = {
-	/* modifier                     key        function        argument */
-	{ Mod1Mask,                     XK_space,  spawn,          {.v = dmenucmd } },
-	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
+	/* modifier			key        function        argument */
+	{ Mod1Mask,			XK_space,	spawn,          {.v = dmenucmd } },
+	{ MODKEY,			XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,			XK_BackSpace,	spawn,		{.v = (const char*[]){ "sysact", NULL } } },
 	{ MODKEY,			XK_b,		spawn,		{.v = (const char*[]){ BROWSER, NULL } } },
 	{ MODKEY,			XK_p,		spawn,		{.v = (const char*[]){ "passmenu", NULL } } },
-	{ 0,							XK_Print,						spawn,			SHCMD ("flameshot gui --clipboard")},
-	{ MODKEY,						XK_Print,						spawn,			SHCMD ("flameshot full -p $HOME/Screenshots/")},
-	{ MODKEY|ControlMask,			XK_Print,						spawn,			SHCMD ("flameshot gui -p $HOME/Screenshots/")},
-	{ MODKEY|ShiftMask,                       XK_b,      togglebar,      {0} },
+	{ 0,				XK_Print,						spawn,			SHCMD ("flameshot gui --clipboard")},
+	{ MODKEY,			XK_Print,						spawn,			SHCMD ("flameshot full -p $HOME/Screenshots/")},
+	{ MODKEY|ControlMask,		XK_Print,						spawn,			SHCMD ("flameshot gui -p $HOME/Screenshots/")},
+	{ MODKEY|ShiftMask,		XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
