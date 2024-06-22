032eeaff7818b2e814f70364d7944e7839e215ce
JirakLu <jirak.lukas@gmail.com>
2024-06-22 23:44:49 +0200

    feat(dmenu): cleanup config

--- dmenu/config.h
+++ dmenu/config.h
@@ -1,24 +1,26 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static int centered = 1;                    /* -c  option; centers dmenu on screen */
-static int min_width = 400;                 /* minimum width when centred */
+static int topbar    = 1;           /* -b  option; if 0, dmenu appears at bottom     */
+static int centered  = 1;           /* -c  option; centers dmenu on screen */
+static int min_width = 400;         /* minimum width when centred */
 
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"JetBrainsMono NF:size=10"
 };
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *prompt = NULL;   /* -p  option; prompt to the left of input field */
+
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	/*                  fg         bg       */
+	[SchemeNorm] =   { "#D8DEE9", "#2E3440" },
+	[SchemeSel] =    { "#ECEFF4", "#434C5E" },
+	[SchemeOut] =    { "#000000", "#00ffff" },
 };
+
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 10;
-static unsigned int columns    = 1;
+static unsigned int lines   = 10;
+static unsigned int columns = 1;
 
 /*
  * Characters not considered part of a word while deleting words
