827287250c75240bcce18ba52426fa11366de359
JirakLu <jirak.lukas@gmail.com>
2024-06-22 23:26:59 +0200

    feat(dmenu): add optimalizations, add gitignore file

--- dmenu/config.mk
+++ dmenu/config.mk
@@ -23,9 +23,16 @@ FREETYPEINC = /usr/include/freetype2
 INCS = -I$(X11INC) -I$(FREETYPEINC)
 LIBS = -L$(X11LIB) -lX11 $(XINERAMALIBS) $(FREETYPELIBS)
 
+# Optional compiler optimisations may create smaller binaries and
+# faster code, but increases compile time.
+# See https://gcc.gnu.org/onlinedocs/gcc/Optimize-Options.html
+OPTIMISATIONS = -march=native -mtune=native -flto=auto -O3
+
 # flags
 CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_XOPEN_SOURCE=700 -D_POSIX_C_SOURCE=200809L -DVERSION=\"$(VERSION)\" $(XINERAMAFLAGS)
-CFLAGS   = -std=c99 -pedantic -Wall -Os $(INCS) $(CPPFLAGS)
+#CFLAGS   = -std=c99 -pedantic -Wall -Os $(INCS) $(CPPFLAGS)
+CFLAGS   = ${OPTIMISATIONS} -std=c99 -pedantic -Wall -Wno-unused-function -Wno-deprecated-declarations ${INCS} ${CPPFLAGS}
+
 LDFLAGS  = $(LIBS)
 
 # compiler and linker
